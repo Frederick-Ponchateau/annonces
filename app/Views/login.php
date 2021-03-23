ced22d916d7c34eeacff8d0c6ce7966676e47873
Frederick-dev23 <79848521+Frederick-dev23@users.noreply.github.com>
2021-03-23 07:58:34 -0400

    ok

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,45 +1,3 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
- 
-    <title>Login</title>
-  </head>
-  <body>
-    <div class="container">
-        <div class="row justify-content-md-center">
- 
-            <div class="col-6">
-                <h1>Sign In</h1>
-                <?php if(session()->getFlashdata('msg')):?>
-                    <div class="alert alert-danger"><?= session()->getFlashdata('msg') ?></div>
-                <?php endif;?>
-                <form action="/login/auth" method="post">
-                    <div class="mb-3">
-                        <label for="InputForEmail" class="form-label">Email address</label>
-                        <input type="email" name="email" class="form-control" id="InputForEmail" value="<?= set_value('email') ?>">
-                    </div>
-                    <div class="mb-3">
-                        <label for="InputForPassword" class="form-label">Password</label>
-                        <input type="password" name="password" class="form-control" id="InputForPassword">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </form>
-            </div>
-             
-        </div>
-    </div>
-     
-    <!-- Popper.js first, then Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-  </body>
-</html>
-
 <!DOCTYPE html>
 <!--
 Template Name: Materialize - Material Design Admin Template
@@ -83,25 +41,30 @@ License: You must have a valid license purchased only from themeforest(the above
     <div class="row">
       <div class="col s12">
         <div class="container"><div id="login-page" class="row">
-  <div class="col s12 m6 l4 z-depth-4 card-panel border-radius-6 login-card bg-opacity-8">
-    <form class="login-form">
+    <div class="col s12 m6 l4 z-depth-4 card-panel border-radius-6 login-card bg-opacity-8">
+   
+    <form class="login-form" action="/login/auth" method="post">
       <div class="row">
         <div class="input-field col s12">
           <h5 class="ml-4">Sign in</h5>
         </div>
+      
       </div>
+      <?php if(session()->getFlashdata('msg')):?>
+                    <div class="alert alert-danger"><?= session()->getFlashdata('msg') ?></div>
+                <?php endif;?>
       <div class="row margin">
         <div class="input-field col s12">
           <i class="material-icons prefix pt-2">person_outline</i>
-          <input id="username" type="text">
-          <label for="username" class="center-align">Username</label>
+          <input id="InputForEmail" type="email" name="email" value="<?= set_value('email') ?>">
+          <label for="InputForEmail" class="center-align">Email address</label>
         </div>
       </div>
       <div class="row margin">
         <div class="input-field col s12">
           <i class="material-icons prefix pt-2">lock_outline</i>
-          <input id="password" type="password">
-          <label for="password">Password</label>
+          <input  name="password" class="form-control" id="InputForPassword" type="password">
+          <label for="InputForPassword">Password</label>
         </div>
       </div>
       <div class="row">
@@ -113,16 +76,14 @@ License: You must have a valid license purchased only from themeforest(the above
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <a href="index.html" class="btn waves-effect waves-light border-round gradient-45deg-purple-deep-orange col s12">Login</a>
+          <button type="submit" class="btn waves-effect waves-light border-round gradient-45deg-purple-deep-orange col s12">Login</button>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6 m6 l6">
-          <p class="margin medium-small"><a href="user-register.html">Register Now!</a></p>
+          <p class="margin medium-small"><a href="<?= base_url("register/")?>">Register Now!</a></p>
         </div>
-        <div class="input-field col s6 m6 l6">
-          <p class="margin right-align medium-small"><a href="user-forgot-password.html">Forgot password ?</a></p>
-        </div>
+       
       </div>
     </form>
   </div>
--- app/Views/register.php
+++ app/Views/register.php
@@ -1,61 +1,139 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<!--
+Template Name: Materialize - Material Design Admin Template
+Author: PixInvent
+Website: http://www.pixinvent.com/
+Contact: hello@pixinvent.com
+Follow: www.twitter.com/pixinvents
+Like: www.facebook.com/pixinvents
+Purchase: https://themeforest.net/item/materialize-material-design-admin-template/11446068?ref=pixinvent
+Renew Support: https://themeforest.net/item/materialize-material-design-admin-template/11446068?ref=pixinvent
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+
+-->
+<html class="loading" lang="en" data-textdirection="ltr">
+  <!-- BEGIN: Head-->
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
- 
-    <title>Register</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google.">
+    <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template, eCommerce dashboard, analytic dashboard">
+    <meta name="author" content="ThemeSelect">
+    <title>User Register </title>
+    <link rel="apple-touch-icon" href="../../../app-assets/images/favicon/apple-touch-icon-152x152.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/favicon/favicon-32x32.png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- BEGIN: VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url("app-assets/vendors/vendors.min.css")?>">
+    <!-- END: VENDOR CSS-->
+    <!-- BEGIN: Page Level CSS-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url("app-assets/css/themes/materialize.css")?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url("app-assets/css/themes/style.css")?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url("app-assets/css/pages/register.css")?>">
+    <!-- END: Page Level CSS-->
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url("app-assets/css/custom/custom.css")?>">
+    <!-- END: Custom CSS-->
   </head>
-  <body>
-    <div class="container">
-        <div class="row justify-content-md-center">
- 
-            <div class="col-6">
-                <h1>Sign Up</h1>
-                <?php if(isset($validation)):?>
-                    <div class="alert alert-danger"><?= $validation->listErrors() ?></div>
-                <?php endif;?>
-                <form action="/register/save" method="post">
-                <div class="mb-3">
-                        <label for="InputForLogin" class="form-label">Login</label>
-                        <input type="text" name="login" class="form-control" id="InputForLogin" value="<?= set_value('login') ?>">
+  <!-- END: Head-->
+  <body class="vertical-layout vertical-menu-collapsible page-header-dark vertical-modern-menu preload-transitions 1-column register-bg   blank-page blank-page" data-open="click" data-menu="vertical-modern-menu" data-col="1-column">
+    <div class="row">
+      <div class="col s12">
+        <div class="container">
+          <div id="register-page" class="row">
+              <div class="col s12 m6 l4 z-depth-4 card-panel border-radius-6 register-card bg-opacity-8">
+                <form class="login-form" action="/register/save" method="post">
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <h5 class="ml-4">Register</h5>
+                      <p class="ml-4">Join to our community now !</p>
+                    
                     </div>
-                    <div class="mb-3">
-                        <label for="InputForName" class="form-label">Nom</label>
-                        <input type="text" name="name" class="form-control" id="InputForName" value="<?= set_value('name') ?>">
+                    <?php if(isset($validation)):?>
+                                <div class="alert alert-danger"><?= $validation->listErrors() ?></div>
+                    <?php endif;?>
+                  </div>
+                  <div class="row margin">
+                    <div class="input-field col s6">
+                      <i class="material-icons prefix pt-2">person_outline</i>
+                      <input id="InputForName" type="text" name="name"value="<?= set_value('name') ?>">
+                      <label for="InputForName"  class="center-align">Nom</label>
                     </div>
-                    <div class="mb-3">
-                        <label for="InputForPrenom" class="form-label">Prenom</label>
-                        <input type="text" name="prenom" class="form-control" id="InputForPrenom" value="<?= set_value('prenom') ?>">
+                 
+                    <div class="input-field col s6">
+                      <i class="material-icons prefix pt-2">person_outline</i>
+                      <input id="InputForPrenom" name="prenom" type="text" value="<?= set_value('prenom') ?>">
+                      <label for="InputForPrenom" class="center-align">Prenom</label>
                     </div>
-                    <div class="mb-3">
-                        <label for="InputForPhone" class="form-label">Phone</label>
-                        <input type="number" name="phone" class="form-control" id="InputForPhone" value="<?= set_value('phone') ?>">
+                  </div>
+                  <div class="row margin">
+                    <div class="input-field col s6">
+                      <?php if(isset($validation)):?>
+                                  <div class="alert alert-danger"><?= $validation->hasError("login") ?></div>
+                      <?php endif;?>
+                      <i class="material-icons prefix pt-2">person_outline</i>
+                      <input id="InputForLogin" value="<?= set_value('login') ?>" name="login" type="text">
+                      <label for="InputForLogin" class="center-align">Login</label>
                     </div>
-                    <div class="mb-3">
-                        <label for="InputForEmail" class="form-label">Email address</label>
-                        <input type="email" name="email" class="form-control" id="InputForEmail" value="<?= set_value('email') ?>">
+                    <div class="input-field col s6">
+                      <i class="material-icons prefix pt-2">person_outline</i>
+                      <input id="InputForPhone" type="number" name="phone" value="<?= set_value('phone') ?>">
+                      <label for="InputForPhone" class="center-align">Phone</label>
                     </div>
-                    <div class="mb-3">
-                        <label for="InputForPassword" class="form-label">Password</label>
-                        <input type="password" name="password" class="form-control" id="InputForPassword">
+                  </div>
+                  <div class="row margin">
+                    <div class="input-field col s12">
+                      <i class="material-icons prefix pt-2">mail_outline</i>
+                      <input id="InputForEmail" type="email" name="email" value="<?= set_value('email') ?>">
+                      <label for="InputForEmail">Email</label>
                     </div>
-                    <div class="mb-3">
-                        <label for="InputForConfPassword" class="form-label">Confirm Password</label>
-                        <input type="password" name="confpassword" class="form-control" id="InputForConfPassword">
+                  </div>
+                  <div class="row margin">
+                    <div class="input-field col s12">
+                      <i class="material-icons prefix pt-2">lock_outline</i>
+                      <input id="InputForPassword" name="password" type="password">
+                      <label for="InputForPassword">Password</label>
                     </div>
-                    <button type="submit" class="btn btn-primary">Register</button>
+                  </div>
+                  <div class="row margin">
+                    <div class="input-field col s12">
+                      <i class="material-icons prefix pt-2">lock_outline</i>
+                      <input id="InputForConfPassword" name="confpassword" type="password">
+                      <label for="InputForConfPassword">Password again</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <button type="submit" class="btn waves-effect waves-light border-round gradient-45deg-purple-deep-orange col s12">Register</button>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <p class="margin medium-small"><a href="<?= base_url('login/')?>">Vous avez deja un compte? Connexion</a></p>
+                    </div>
+                  </div>
                 </form>
-            </div>
-             
+              </div>
+          </div>
         </div>
+        <div class="content-overlay"></div>
+      </div>
     </div>
-     
-    <!-- Popper.js first, then Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+
+    <!-- BEGIN VENDOR JS-->
+    <script src="<?=base_url("app-assets/js/vendors.min.js")?>"></script>
+    <!-- BEGIN VENDOR JS-->
+    <!-- BEGIN PAGE VENDOR JS-->
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN THEME  JS-->
+    <script src="<?=base_url("app-assets/js/plugins.js")?>"></script>
+    <script src="<?=base_url("app-assets/js/search.js")?>"></script>
+    <script src="<?=base_url("app-assets/js/custom/custom-script.js")?>"></script>
+    <!-- END THEME  JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <!-- END PAGE LEVEL JS-->
   </body>
 </html>
+
+
